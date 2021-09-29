2bc75070567de2910ac288f012b611c803403f06
CuTe_PsYcHo_110 <89610199+CuTePsYcHo110@users.noreply.github.com>
2021-09-29 21:06:45 +0300

    Add files via upload

---
+++ Bot/plugins/broadcast.py
@@ -0,0 +1,32 @@
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from bot import AUTH_CHANNEL, COMMM_AND_PRE_FIX, BROADCAST_COMMAND
+from bot.bot import Bot
+from bot.hf.flifi import uszkhvis_chats_ahndler
+from bot.sql.users_sql import get_chats
+
+
+@Bot.on_message(
+    filters.command(BROADCAST_COMMAND, COMMM_AND_PRE_FIX)
+    & uszkhvis_chats_ahndler([AUTH_CHANNEL])
+)
+async def num_start_message(client: Bot, message: Message):
+    if not message.reply_to_message:
+        return await message.reply_text("Reply to Message..", quote=True)
+    reply = message.reply_to_message
+    All = get_chats()
+    TTL = len(All)
+    SUCCESS = 0
+    for chat in All:
+        try:
+            await reply.copy(chat)
+            SUCCESS += 1
+        except Exception as e:
+            print(e, chat)
+    MSG = "**BroadCast Completed !**\n"
+    MSG += f"Succeed : {SUCCESS} Chats!"
+    if TTL != SUCCESS:
+        MSG += f"\nFailed : {All-SUCCESS} Chats."
+    await message.reply_text(MSG, quote=True)
---
+++ Bot/plugins/tg_derps.py
@@ -0,0 +1,48 @@
+
+
+
+from pyrogram.types import (
+    CallbackQuery,
+    Message
+)
+from typing import List
+from bot import (
+    AUTH_CHANNEL,
+    DELETED_MESSAGES_NOTIFICATION_TEXT,
+    DERP_USER_S_TEXT
+)
+from bot.bot import Bot
+from bot.sql.users_sql import (
+    get_chek_dmid,
+    get_chek_mdid
+)
+
+
+@Bot.on_deleted_messages()
+async def on_del_mesgs(client: Bot, messages: List[Message]):
+    for message in messages:
+        if message.chat is not None:
+            kopp = get_chek_mdid(message.message_id)
+            if kopp:
+                await client.delete_messages(
+                    chat_id=int(kopp.chat_id),
+                    message_ids=kopp.mu_id,
+                    revoke=True
+                )
+        else:
+            ym = get_chek_dmid(message.message_id)
+            if ym:
+                await client.send_message(
+                    chat_id=AUTH_CHANNEL,
+                    text=DELETED_MESSAGES_NOTIFICATION_TEXT,
+                    reply_to_message_id=ym.message_id
+                )
+
+
+@Bot.on_callback_query()
+async def on_cb_qry(_, callback_query: CallbackQuery):
+    await callback_query.answer(
+        text=DERP_USER_S_TEXT,
+        show_alert=False,
+        cache_time=0
+    )
