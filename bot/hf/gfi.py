099c23c6395061e266149a9f4210c325aa548383
CuTe_PsYcHo_110 <89610199+CuTePsYcHo110@users.noreply.github.com>
2021-09-30 09:04:34 +0300

    Add files via upload

---
+++ bot/hf/gfi.py
@@ -0,0 +1,25 @@
+
+
+from pyrogram.types import Message
+
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                return obj, obj.file_id
