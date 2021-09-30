b5dc90c1c4076f5dedf579d88b703b2c08011381
CuTe_PsYcHo_110 <89610199+CuTePsYcHo110@users.noreply.github.com>
2021-09-30 09:02:12 +0300

    Create get_config.py

---
+++ bot/get_config.py
@@ -0,0 +1,16 @@
+""" wrapper for getting the credentials """
+
+import os
+
+
+def get_config(name: str, d_v=None, should_prompt=False):
+    """ accepts one mandatory variable
+    and prompts for the value, if not available """
+    val = os.environ.get(name, d_v)
+    if not val and should_prompt:
+        try:
+            val = input(f"enter {name}'s value: ")
+        except EOFError:
+            val = d_v
+        print("\n")
+    return val
