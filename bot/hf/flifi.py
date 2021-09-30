a88fe116158e3c06bc526f1f45f85087cf7a5b3f
CuTe_PsYcHo_110 <89610199+CuTePsYcHo110@users.noreply.github.com>
2021-09-30 09:04:15 +0300

    Add files via upload

---
+++ bot/hf/flifi.py
@@ -0,0 +1,21 @@
+
+from typing import List, Union
+from pyrogram import (
+    Client,
+    filters
+)
+from pyrogram.types import (
+    Message
+)
+
+
+def uszkhvis_chats_ahndler(chats: List[Union[str, int]]):
+    async def func(flt, client: Client, message: Message):
+        chats = flt.chats
+        return bool(
+            message.chat and (
+                message.chat.id in chats
+            )
+        )
+    # "chats" kwarg is accessed with "flt.chats" above
+    return filters.create(func, chats=chats)
